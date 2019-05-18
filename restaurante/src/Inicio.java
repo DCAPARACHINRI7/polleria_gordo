ccee42dd892e6d2cf1b2898dd5664f9e2d92f490
DAVID <dcr57312@gmail.com>
2019-05-17 20:49:19 -0500

    ingresando los botones

--- restaurante/src/Inicio.java
+++ restaurante/src/Inicio.java
@@ -26,21 +26,103 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        logo = new javax.swing.JLabel();
+        usuario = new javax.swing.JLabel();
+        barra = new javax.swing.JLabel();
+        candado = new javax.swing.JLabel();
+        barra2 = new javax.swing.JLabel();
+        visible = new javax.swing.JButton();
+        noVisible = new javax.swing.JButton();
+        olvidemicontraseña = new javax.swing.JLabel();
+        ingresar = new javax.swing.JButton();
+        regustrar = new javax.swing.JButton();
+        fondo = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/LogoPequeño.png"))); // NOI18N
+        getContentPane().add(logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(831, 30, 260, 220));
+
+        usuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/IMG_Usuario.png"))); // NOI18N
+        getContentPane().add(usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(808, 300, 20, 30));
+
+        barra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Separador.png"))); // NOI18N
+        getContentPane().add(barra, new org.netbeans.lib.awtextra.AbsoluteConstraints(808, 325, 330, 10));
+
+        candado.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/IMG_Candado.png"))); // NOI18N
+        getContentPane().add(candado, new org.netbeans.lib.awtextra.AbsoluteConstraints(808, 370, 20, 30));
+
+        barra2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Separador.png"))); // NOI18N
+        getContentPane().add(barra2, new org.netbeans.lib.awtextra.AbsoluteConstraints(808, 395, 350, 10));
+
+        visible.setForeground(new java.awt.Color(255, 255, 255));
+        visible.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/IMG_Visible.png"))); // NOI18N
+        visible.setBorder(null);
+        visible.setBorderPainted(false);
+        visible.setContentAreaFilled(false);
+        visible.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                visibleActionPerformed(evt);
+            }
+        });
+        getContentPane().add(visible, new org.netbeans.lib.awtextra.AbsoluteConstraints(1100, 375, 30, 20));
+
+        noVisible.setForeground(new java.awt.Color(255, 255, 255));
+        noVisible.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/vista_tapada.png"))); // NOI18N
+        noVisible.setBorder(null);
+        noVisible.setBorderPainted(false);
+        noVisible.setContentAreaFilled(false);
+        noVisible.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                noVisibleActionPerformed(evt);
+            }
+        });
+        getContentPane().add(noVisible, new org.netbeans.lib.awtextra.AbsoluteConstraints(1100, 375, 30, 20));
+
+        olvidemicontraseña.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        olvidemicontraseña.setForeground(new java.awt.Color(168, 12, 17));
+        olvidemicontraseña.setText("¿Olvidaste tu contraseña?");
+        olvidemicontraseña.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                olvidemicontraseñaMouseClicked(evt);
+            }
+        });
+        getContentPane().add(olvidemicontraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(890, 414, 160, 20));
+
+        ingresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/btn_Ingresar.png"))); // NOI18N
+        ingresar.setBorder(null);
+        ingresar.setBorderPainted(false);
+        ingresar.setContentAreaFilled(false);
+        ingresar.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/img/btn_IngresarSecundario.png"))); // NOI18N
+        getContentPane().add(ingresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(804, 482, 340, 50));
+
+        regustrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/btn_Registrarse.png"))); // NOI18N
+        regustrar.setBorder(null);
+        regustrar.setBorderPainted(false);
+        regustrar.setContentAreaFilled(false);
+        regustrar.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/img/btn_RegistrarseSecundario.png"))); // NOI18N
+        getContentPane().add(regustrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 560, 340, 50));
+
+        fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/FondoIzquierdo.png"))); // NOI18N
+        getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void visibleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_visibleActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_visibleActionPerformed
+
+    private void noVisibleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_noVisibleActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_noVisibleActionPerformed
+
+    private void olvidemicontraseñaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_olvidemicontraseñaMouseClicked
+        
+        
+        /*A qui se colocara a donde se quiere mandar*/
+    }//GEN-LAST:event_olvidemicontraseñaMouseClicked
 
     /**
      * @param args the command line arguments
@@ -78,5 +160,16 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel barra;
+    private javax.swing.JLabel barra2;
+    private javax.swing.JLabel candado;
+    private javax.swing.JLabel fondo;
+    private javax.swing.JButton ingresar;
+    private javax.swing.JLabel logo;
+    private javax.swing.JButton noVisible;
+    private javax.swing.JLabel olvidemicontraseña;
+    private javax.swing.JButton regustrar;
+    private javax.swing.JLabel usuario;
+    private javax.swing.JButton visible;
     // End of variables declaration//GEN-END:variables
 }
--- restaurante/src/Splash_Screen.java
+++ restaurante/src/Splash_Screen.java
@@ -26,17 +26,41 @@ public class Splash_Screen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Logo.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(393, 393, 393)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 487, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(400, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(109, 109, 109)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE)
+                .addGap(205, 205, 205))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -78,5 +102,7 @@ public class Splash_Screen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
